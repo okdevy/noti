21239907cc7cfc385d70f914f9d6d83500cd8418
o.krynytskyi <o.krynytskyi.bekey@gmail.com>
2024-01-18 21:29:43 +0200

    chore: cleanup

--- app/macos/Flutter/GeneratedPluginRegistrant.swift
+++ app/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,12 +7,14 @@ import Foundation
 
 import connectivity_plus
 import file_selector_macos
+import flutter_local_notifications
 import path_provider_foundation
 import window_size
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   ConnectivityPlugin.register(with: registry.registrar(forPlugin: "ConnectivityPlugin"))
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
+  FlutterLocalNotificationsPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalNotificationsPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   WindowSizePlugin.register(with: registry.registrar(forPlugin: "WindowSizePlugin"))
 }
